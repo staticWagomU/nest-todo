037b696610cdab56c7755aae7dff93154f845dcb
staticWagomU <to6wa17@gmail.com>
2024-12-02 23:49:00 +0900

    feat: add routing '/', '/hello' and '/goodbye'

--- app/backend/src/app.controller.ts
+++ app/backend/src/app.controller.ts
@@ -5,8 +5,18 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/goodbye')
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
+  }
+
+  @Get()
+  index(): string {
+    return this.appService.index();
   }
 }
--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getGoodbye(): string {
+    return 'Goodbye!';
+  }
+  index(): string {
+    return 'Index page';
+  }
 }
