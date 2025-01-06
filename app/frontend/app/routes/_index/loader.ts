fd5b8f4ce061d8837f2aa40fe8decd9aaf20c426
staticWagomU <to6wa17@gmail.com>
2025-01-06 19:38:02 +0900

    feat: valibotを使ってフェッチデータを検証

--- app/frontend/app/routes/_index/loader.ts
+++ app/frontend/app/routes/_index/loader.ts
@@ -1,10 +1,26 @@
+import * as v from "valibot";
+
+const TodoSchema = v.array(
+  v.object({
+    id: v.pipe(v.string(), v.uuid()),
+    title: v.string(),
+    description: v.string(),
+    completed: v.boolean(),
+  })
+);
+
 export const TodoLoader = async () => {
   const res = await fetch('http://localhost:3000/todos');
   if (!res.ok) {
     throw new Response(null, { status: 404 });
   }
-
-  return await res.json();
+  const data = await res.json();
+  try {
+    const validData = v.parse(TodoSchema, data);
+    return validData;
+  }catch(e) {
+    throw new Response(null, { status: 500 });
+  }
 };
 
 export type TodoLoader = typeof TodoLoader;
